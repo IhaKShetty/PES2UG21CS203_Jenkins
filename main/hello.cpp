21874db6e58806dd0567a4a31987665cbfff1d23
Iha <120178605+IhaKShetty@users.noreply.github.com>
2024-03-10 23:54:16 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, t\n";
+  cout << "Hello, world\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "This is Iha; successfully run\n";
   return 0;
 }
